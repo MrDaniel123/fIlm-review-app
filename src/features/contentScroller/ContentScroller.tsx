b6f16f1e900e32a622461beb1b948e2ba6158084
Daniel <pandaniel13286@gmail.com>
2024-01-29 10:50:32 +0100

    Change Arrow in contentScroling

--- src/features/contentScroller/ContentScroller.tsx
+++ src/features/contentScroller/ContentScroller.tsx
@@ -50,12 +50,16 @@ const ScrollButton = styled.button<{ type?: string }>`
 	all: unset;
 	position: absolute;
 	top: 80px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 	cursor: pointer;
 	height: 140px;
+	width: 30px;
 	border-radius: 0 25px 25px 0;
 
 	& img {
-		width: 30px;
+		width: 120px;
 	}
 	${props =>
 		props.type === 'right'
